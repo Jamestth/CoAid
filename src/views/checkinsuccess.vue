b559269c10b0f67cd760b9844758d3b6b6b581e2
GeoffreyLion10 <geoffreylion67@gmail.com>
2020-10-21 17:48:00 +0800

    signin

---
+++ src/views/checkinsuccess.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="checkinsuccess">
+    <table class = "center" style="text-align: center">
+        <tr>
+            <td> <img src = "../assets/checkin.jpg" class = "left" style = "width:50%"> </td>
+        </tr>
+        <tr>
+            <td> Your check in was successful. </td>
+        </tr>
+        <tr>
+            <td> Have a nice day! </td>
+        </tr>
+        <tr>
+            <td> <button type="button"> Home </button> </td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style scoped>
+div {
+  text-align: left;
+  padding: 15px;
+}
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
