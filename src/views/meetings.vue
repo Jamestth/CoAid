e33e3a77d447a15e84a97ed0e1dbdc9bf1c24107
James <jamestth97@gmail.com>
2020-10-29 01:17:54 +0800

    Touched up meetings added modal

--- src/views/meetings.vue
+++ src/views/meetings.vue
@@ -1,45 +1,63 @@
 <template>
-    <b-container class="meetingspage">
-    <h1> Upcoming Meetings <router-link
-            to="/createmeeting"
-            tag="button"
-            class="btn ml-3"
-            style="margin:0"
-            >Create meeting</router-link
-          ></h1>
+  <b-container class="meetingspage">
+    <h1>
+      Upcoming Meetings
+      <router-link
+        to="/createmeeting"
+        tag="button"
+        class="btn ml-3"
+        style="margin:0"
+        >Create meeting</router-link
+      >
+    </h1>
     <div class="Meetings">
       <b-row class="text-center">
-          <b-table striped hover :items="items"></b-table>
-             
+        <b-table striped hover :items="items"></b-table>
       </b-row>
-      </div>
-    </b-container>
-    
+    </div>
+  </b-container>
 </template>
 
 <script>
-import Multiselect from "vue-multiselect";
-
 export default {
-  components: { Multiselect },
   data() {
-      return {
-        items: [
-          { 'Date DDMMYY': '25/11/20', time: '14:00', 'Meeting Subject': 'Marketing & Product Launch Meeting', status: 'ACCEPTED' },
-          { 'Date DDMMYY': '4/12/20', time: '10:00', 'Meeting Subject': 'Employee Appraisal', status: 'ACCEPTED' },
-          { 'Date DDMMYY': '7/01/21', time: '15:00', 'Meeting Subject': 'Project Aspire Stage 2', status: 'PENDING' },
-          { 'Date DDMMYY': '01/10/20', time: '9:30', 'Meeting Subject': 'Board of Directors Meeting', status: 'PAST' }
-      ]
-    }
-  }
-}
+    return {
+      items: [
+        {
+          "Date DDMMYY": "25/11/20",
+          time: "14:00",
+          "Meeting Subject": "Marketing & Product Launch Meeting",
+          status: "ACCEPTED",
+        },
+        {
+          "Date DDMMYY": "4/12/20",
+          time: "10:00",
+          "Meeting Subject": "Employee Appraisal",
+          status: "ACCEPTED",
+        },
+        {
+          "Date DDMMYY": "7/01/21",
+          time: "15:00",
+          "Meeting Subject": "Project Aspire Stage 2",
+          status: "PENDING",
+        },
+        {
+          "Date DDMMYY": "01/10/20",
+          time: "9:30",
+          "Meeting Subject": "Board of Directors Meeting",
+          status: "PAST",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500");
 h1 {
-  font-family: 'Josefin Sans', sans-serif !important;
+  font-family: "Josefin Sans", sans-serif !important;
   padding: 15px;
   font-size: 30px;
   text-align: center;
@@ -62,7 +80,9 @@ div {
   border-radius: 5px;
   background: #fff;
 }
-.multiselect, .multiselect_input, .multiselect_single {
+.multiselect,
+.multiselect_input,
+.multiselect_single {
   font-family: inherit;
   font-size: 15px;
   padding: 15px;
@@ -81,18 +101,15 @@ div {
   margin: auto;
   font-family: Arial, Helvetica, sans-serif;
   width: 100px;
-  border-radius: 8px
-
+  border-radius: 8px;
 }
 .btn:hover {
-    background-color: white;
-      border-color: darkblue;
-      color: darkblue;
+  background-color: white;
+  border-color: darkblue;
+  color: darkblue;
 }
 
 label {
   margin: 10px;
-
 }
-
 </style>
