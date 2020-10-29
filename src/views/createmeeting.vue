cf90dc38aaf5adbe02bce26b832abdcd436794fd
Denise <E0439348@u.nus.edu>
2020-10-29 18:08:22 +0800

    create a meeting test 2

--- src/views/createmeeting.vue
+++ src/views/createmeeting.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="meetingspage">
     <h1>
-      Create a Meeting test
+      Create a Meeting test2
       <router-link to="/meetings" tag="button" class="btn ml-3" style="margin:0"
         >Back to meetings</router-link
       >
