895cfab80f4175db897628e4ad230a310dd2dbc8
pwaaron <matheusaaron09@gmail.com>
2020-11-03 01:34:06 +0800

    Updates router to not bounce on every page

--- src/router/index.js
+++ src/router/index.js
@@ -15,67 +15,80 @@ const routes = [
     path: "/analytics",
     name: "Analytics",
     component: () => import("../views/analytics.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/checkinout",
     name: "Checkinout",
     component: () => import("../views/checkinout.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/meetings",
     name: "Meetings",
     component: () => import("../views/meetings.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/loginsignup",
     name: "loginsignup",
     component: () => import("../views/loginsignup.vue"),
+    meta: { requiresAuth: false },
   },
   {
     path: "/signup",
     name: "signup",
     component: () => import("../views/signup.vue"),
+    meta: { requiresAuth: false },
   },
   {
     path: "/login",
     name: "login",
     component: () => import("../views/login.vue"),
+    meta: { requiresAuth: false },
   },
   {
     path: "/schedule",
     name: "Schedule",
     component: () => import("../views/schedule.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/checkinsuccess",
     name: "Checkinsuccess",
     component: () => import("../views/checkinsuccess.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/healthdeclaration",
     name: "healthdeclaration",
     component: () => import("../views/healthdeclaration.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/checkoutsuccess",
     name: "Checkoutsuccess",
     component: () => import("../views/checkoutsuccess.vue"),
+    meta: { requiresAuth: false },
   },
   {
     path: "/profile/:id/:personal",
     name: "Profile",
     component: () => import("../views/profile.vue"),
+    meta: { requiresAuth: true },
     props: true,
   },
   {
     path: "/createmeeting",
     name: "Createmeeting",
     component: () => import("../views/createmeeting.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/meetingsuccess",
     name: "Meetingsuccess",
     component: () => import("../views/meetingsuccess.vue"),
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -88,7 +101,8 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   let isLoggedIn = auth.currentUser;
   //isLoggedIn = true; // remove when auth is done
-  if (!isLoggedIn && to.path !== "/login") {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth && !isLoggedIn) {
     next("/login");
   } else if (isLoggedIn && to.path === "/login") {
     next("/");
