778e17b091a5b5e0818a6b0ac921d7560a5b0c87
pwaaron <matheusaaron09@gmail.com>
2020-11-14 03:01:46 +0800

    Fixed odd/even bug

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -6,7 +6,9 @@
         <br />
         <h2>Here's your schedule, {{ this.userInfo.data().name }}</h2>
         <br />
-        <h1 v-if="this.name != ''">You are in Roster: <b>{{ this.name }}</b></h1>
+        <h1 v-if="this.name != ''">
+          You are in Roster: <b>{{ this.name }}</b>
+        </h1>
         <h1 v-else>You are not in any roster</h1>
       </b-col>
       <b-col></b-col>
@@ -36,9 +38,9 @@
       <b-col> </b-col>
     </b-row>
     <b-row>
-      <b-col/>
+      <b-col />
       <b-col cols="8">
-        <h2> Check out other rosters! </h2>
+        <h2>Check out other rosters!</h2>
         <b-form-select
           v-model="activeRoster"
           @change.native="updateEmp"
@@ -59,7 +61,7 @@
           </b-list-group-item>
         </b-list-group>
       </b-col>
-      <b-col/>
+      <b-col />
     </b-row>
   </div>
 </template>
@@ -67,11 +69,12 @@
 <script>
 import Calendar from "v-calendar/lib/components/calendar.umd";
 import { auth, database } from "../assets/firebase";
+// import { DateTime } from "luxon";
 
 // Or just use in separate component
 export default {
   components: {
-    Calendar,
+    Calendar
   },
   data() {
     return {
@@ -82,13 +85,13 @@ export default {
       employees: [],
       rosters: [],
       otherEmployees: [],
-      activeRoster: "",
+      activeRoster: ""
     };
   },
   watch: {
-    activeRoster: function () {
+    activeRoster: function() {
       this.updateEmp();
-    },
+    }
   },
   created() {
     this.fetchdata();
@@ -100,8 +103,8 @@ export default {
       database
         .collection("employees")
         .get()
-        .then((emps) =>
-          emps.forEach((emp) => {
+        .then(emps =>
+          emps.forEach(emp => {
             if (emp.data().uid == userId) {
               this.userInfo = emp;
             }
@@ -111,30 +114,60 @@ export default {
       database
         .collection("rosters")
         .get()
-        .then((rosters) => {
-          rosters.forEach((roster) => {
+        .then(rosters => {
+          rosters.forEach(roster => {
             let rst = {
               value: roster.id,
-              text: roster.data().name,
+              text: roster.data().name
             };
             this.rosters.push(rst);
             let emps = roster.data().selectedEmp;
 
-            if (emps.map((x) => x.id).includes(this.userInfo.id)) {
+            if (emps.map(x => x.id).includes(this.userInfo.id)) {
               this.name = roster.data().name;
-              emps.forEach((x) => {
-                x.get().then((x) => {
+              emps.forEach(x => {
+                x.get().then(x => {
                   this.employees.push(x.data().name);
                 });
               });
+              // let today = DateTime.fromISO(DateTime.local().toISODate());
+
+              // Copy date so don't modify original
+              // let d = new Date(Date.UTC(today.year, today.month, today.day));
+              let d = new Date(roster.data().startDate);
+              console.log(d);
+              // Set to nearest Thursday: current date + 4 - current day number
+              // Make Sunday's day number 7
+              d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+              // Get first day of year
+              var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+              // Calculate full weeks to nearest Thursday
+              var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+              // Return array of year and week number
+              console.log(d);
+              console.log(weekNo);   
+
+              var sd = new Date(roster.data().startDate);
+              if(roster.data().schedule == "ODD"){
+                if(weekNo%2==0){
+                  sd.setUTCDate(d.getUTCDate() + 7 - d.getUTCDay());
+                }
+              }
+              else{
+                if(weekNo%2==1){
+                  sd.setUTCDate(d.getUTCDate() + 7 - d.getUTCDay());
+                }
+              }
+              console.log(sd);
+
               let attr = {
                 highlight: "blue",
                 dates: {
-                  start: new Date(roster.data().startDate),
+                  start: sd,
                   end: new Date(roster.data().endDate),
                   weekdays: [2, 3, 4, 5, 6],
-                  weeklyInterval: 2,
-                },
+                  weeklyInterval: 2
+                }
               };
               this.selectedEmp.push(this.userInfo);
               this.attrs.push(attr);
@@ -149,12 +182,12 @@ export default {
       database
         .collection("rosters")
         .get()
-        .then((rosters) => {
-          rosters.forEach((roster) => {
+        .then(rosters => {
+          rosters.forEach(roster => {
             if (roster.id == this.activeRoster) {
               let emps = roster.data().selectedEmp;
-              emps.forEach((x) => {
-                x.get().then((x) => {
+              emps.forEach(x => {
+                x.get().then(x => {
                   if (!this.otherEmployees.includes(x.data().name)) {
                     this.otherEmployees.push(x.data().name);
                   }
@@ -163,7 +196,7 @@ export default {
             }
           });
         });
-    },
-  },
+    }
+  }
 };
 </script>
