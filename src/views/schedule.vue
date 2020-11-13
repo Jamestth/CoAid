2f67a74767e160bca99b988e23f500a842c559fc
Jamestth <65338282+Jamestth@users.noreply.github.com>
2020-11-13 18:21:08 +0800

    solved bug for not updating

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -13,15 +13,16 @@
     <b-row>
       <b-col></b-col>
       <b-col cols="4">
-        <h3> Calendar </h3>
-        <Calendar color="blue" :attributes="attrs" is-expanded is-dark/>
+        <h3>Calendar</h3>
+        <Calendar color="blue" :attributes="attrs" is-expanded is-dark />
       </b-col>
 
       <b-col cols="4">
-        <h3> Roster Members </h3>
+        <h3>Roster Members</h3>
         <br />
         <b-list-group>
-          <b-list-group-item variant="dark"
+          <b-list-group-item
+            variant="dark"
             v-for="employee in employees"
             :name="employee"
             :key="employee"
@@ -30,15 +31,23 @@
           </b-list-group-item>
         </b-list-group>
       </b-col>
-      <b-col>
-      </b-col>
+      <b-col> </b-col>
     </b-row>
     <b-row>
       <b-col>
-        <b-form-select v-model="activeRoster" @change.native="updateEmp" id="cluster" text="Select Cluster" variant="dark" class="m-2" :options="rosters">
+        <b-form-select
+          v-model="activeRoster"
+          @change.native="updateEmp"
+          id="cluster"
+          text="Select Cluster"
+          variant="dark"
+          class="m-2"
+          :options="rosters"
+        >
         </b-form-select>
         <b-list-group :key="activeRoster">
-          <b-list-group-item variant="dark"
+          <b-list-group-item
+            variant="dark"
             v-for="employee in otherEmployees"
             :key="employee"
           >
@@ -57,7 +66,7 @@ import { auth, database } from "../assets/firebase";
 // Or just use in separate component
 export default {
   components: {
-    Calendar
+    Calendar,
   },
   data() {
     return {
@@ -67,11 +76,15 @@ export default {
       selectedEmp: [],
       employees: [],
       rosters: [],
-      otherEmployees:[],
+      otherEmployees: [],
       activeRoster: "",
     };
   },
-
+  watch: {
+    activeRoster: function () {
+      this.updateEmp();
+    },
+  },
   created() {
     this.fetchdata();
   },
@@ -82,8 +95,8 @@ export default {
       database
         .collection("employees")
         .get()
-        .then(emps =>
-          emps.forEach(emp => {
+        .then((emps) =>
+          emps.forEach((emp) => {
             if (emp.data().uid == userId) {
               this.userInfo = emp;
             }
@@ -93,19 +106,19 @@ export default {
       database
         .collection("rosters")
         .get()
-        .then(rosters => {
-          rosters.forEach(roster => {
+        .then((rosters) => {
+          rosters.forEach((roster) => {
             let rst = {
               value: roster.id,
-              text: roster.data().name
-            }
-            this.rosters.push(rst)
+              text: roster.data().name,
+            };
+            this.rosters.push(rst);
             let emps = roster.data().selectedEmp;
 
-            if (emps.map(x => x.id).includes(this.userInfo.id)) {
+            if (emps.map((x) => x.id).includes(this.userInfo.id)) {
               this.name = roster.data().name;
-              emps.forEach(x => {
-                x.get().then(x => {
+              emps.forEach((x) => {
+                x.get().then((x) => {
                   this.employees.push(x.data().name);
                 });
               });
@@ -115,8 +128,8 @@ export default {
                   start: new Date(roster.data().startDate),
                   end: new Date(roster.data().endDate),
                   weekdays: [2, 3, 4, 5, 6],
-                  weeklyInterval: 2
-                }
+                  weeklyInterval: 2,
+                },
               };
               this.selectedEmp.push(this.userInfo);
               this.attrs.push(attr);
@@ -124,23 +137,28 @@ export default {
           });
         });
     },
-    updateEmp(){
-      console.log(length(this.otherEmployees));
+    updateEmp() {
+      //console.log(this.otherEmployees);
+
       this.otherEmployees = [];
       database
         .collection("rosters")
         .get()
-        .then(roster => {
-          if(roster.id == this.activeRoster){
-            let emps = roster.data().selectedEmp;
-            emps.forEach(x => {
-                x.get().then(x => {
-                  this.otherEmployees.push(x.data().name);
+        .then((rosters) => {
+          rosters.forEach((roster) => {
+            if (roster.id == this.activeRoster) {
+              let emps = roster.data().selectedEmp;
+              emps.forEach((x) => {
+                x.get().then((x) => {
+                  if (!this.otherEmployees.includes(x.data().name)) {
+                    this.otherEmployees.push(x.data().name);
+                  }
                 });
               });
-          }
+            }
+          });
         });
-    }
-  }
+    },
+  },
 };
 </script>
